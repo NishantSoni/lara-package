10ab6380024783811d509124011163c1e7734c0f
Nishant Soni <n.soni@easternenterprise.com>
2018-04-23 11:28:13 +0530

    updated

--- src/ArtisanRemoverServiceProvider.php
+++ src/ArtisanRemoverServiceProvider.php
@@ -2,29 +2,147 @@
 
 namespace ArtisanRemover;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Providers\ArtisanServiceProvider as IlluminateProvider;
+use Illuminate\Console\Scheduling\ScheduleRunCommand;
+use Illuminate\Console\Scheduling\ScheduleFinishCommand;
 
-class ArtisanRemoverServiceProvider extends ServiceProvider
+class ArtisanRemoverServiceProvider extends IlluminateProvider
 {
 	/**
-	 * Perform post-registration booting of services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-	    $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-	}
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'CacheClear' => 'command.cache.clear',
+        'CacheForget' => 'command.cache.forget',
+        'ClearCompiled' => 'command.clear-compiled',
+        'ClearResets' => 'command.auth.resets.clear',
+        'ConfigCache' => 'command.config.cache',
+        'ConfigClear' => 'command.config.clear',
+        'Down' => 'command.down',
+        'Environment' => 'command.environment',
+        'KeyGenerate' => 'command.key.generate',
+        'Migrate' => 'command.migrate',
+        'MigrateFresh' => 'command.migrate.fresh',
+        'MigrateInstall' => 'command.migrate.install',
+        'MigrateRefresh' => 'command.migrate.refresh',
+        'MigrateReset' => 'command.migrate.reset',
+        'MigrateRollback' => 'command.migrate.rollback',
+        'MigrateStatus' => 'command.migrate.status',
+        'Optimize' => 'command.optimize',
+        'PackageDiscover' => 'command.package.discover',
+        'Preset' => 'command.preset',
+        'QueueFailed' => 'command.queue.failed',
+        'QueueFlush' => 'command.queue.flush',
+        'QueueForget' => 'command.queue.forget',
+        'QueueListen' => 'command.queue.listen',
+        'QueueRestart' => 'command.queue.restart',
+        'QueueRetry' => 'command.queue.retry',
+        'QueueWork' => 'command.queue.work',
+        'RouteCache' => 'command.route.cache',
+        'RouteClear' => 'command.route.clear',
+        'RouteList' => 'command.route.list',
+        'Seed' => 'command.seed',
+        'ScheduleFinish' => ScheduleFinishCommand::class,
+        'ScheduleRun' => ScheduleRunCommand::class,
+        'StorageLink' => 'command.storage.link',
+        'Up' => 'command.up',
+        'ViewClear' => 'command.view.clear',
+    ];
 
-	/**
-	 * Register bindings in the container.
-	 *
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $devCommands = [
+        'AppName' => 'command.app.name',
+        'AuthMake' => 'command.auth.make',
+        'CacheTable' => 'command.cache.table',
+        'ConsoleMake' => 'command.console.make',
+        'ControllerMake' => 'command.controller.make',
+        'EventGenerate' => 'command.event.generate',
+        'EventMake' => 'command.event.make',
+        'ExceptionMake' => 'command.exception.make',
+        'FactoryMake' => 'command.factory.make',
+        'JobMake' => 'command.job.make',
+        'ListenerMake' => 'command.listener.make',
+        'MailMake' => 'command.mail.make',
+        'MiddlewareMake' => 'command.middleware.make',
+        'MigrateMake' => 'command.migrate.make',
+        'ModelMake' => 'command.model.make',
+        'NotificationMake' => 'command.notification.make',
+        'NotificationTable' => 'command.notification.table',
+        'PolicyMake' => 'command.policy.make',
+        'ProviderMake' => 'command.provider.make',
+        'QueueFailedTable' => 'command.queue.failed-table',
+        'QueueTable' => 'command.queue.table',
+        'RequestMake' => 'command.request.make',
+        'ResourceMake' => 'command.resource.make',
+        'RuleMake' => 'command.rule.make',
+        'SeederMake' => 'command.seeder.make',
+        'SessionTable' => 'command.session.table',
+        'Serve' => 'command.serve',
+        'TestMake' => 'command.test.make',
+        'VendorPublish' => 'command.vendor.publish',
+    ];
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    	// Remove the commands from artisan commands list, if exist.
+    	$this->removeArtisanCommands();
+
+        $this->registerCommands(array_merge(
+            $this->commands, $this->devCommands
+        ));
+    }
+
+    /**
+     * Register the given commands, and remove the commands which are mentioned in the ENV.
+     *
+     * @param  array  $commands
+     * @return void
+     */
+    protected function registerCommands(array $commands)
+    {
+    	foreach (array_keys($commands) as $command) {
+            call_user_func_array([$this, "register{$command}Command"], []);
+        }
+
+        $this->commands(array_values($commands));
+    }
+
+    /**
+	 * Get the commands from ENV.
+	 * If Exist, check the commands and remove from the artisan commands list. 
+	 * 
 	 * @return void
 	 */
-	public function register()
-	{
-	   
-	}
+    private function removeArtisanCommands()
+    {
+    	$cmds = env('REMOVE_COMMANDS');
+    	if(strlen($cmds) > 0){
+    
+		    $cmdsArray = explode(',' , $cmds);
+		    
+		    if(!empty($cmdsArray)){
+		    
+		        foreach($cmdsArray as $cmdVal){
+		            
+		            $cmd = 'command.'.str_replace(":",".",$cmdVal);
+		            $key = array_search($cmd , $this->commands);
+
+		            unset($this->commands[$key]);
+		        }
+		    }
+		}
+    }
 }
 
 
